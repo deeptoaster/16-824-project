97475e1aeaf9fb5d42783729b3a46ac5d6228d52
Deep Toaster <deeptoaster@gmail.com>
2023-04-20 01:46:09 -0400

    better interface for running dreambooth

--- run_dreambooth.py
+++ run_dreambooth.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python
 import accelerate
-from argparse import Namespace
+from argparse import ArgumentParser, Namespace
 from diffusers import AutoencoderKL, UNet2DConditionModel
-from io import BytesIO
 import itertools
 from pathlib import Path
-from PIL import Image
-import requests
 import torch
 from transformers import CLIPTextModel, CLIPTokenizer
 
-from train import train
+from train_dreambooth import train
+
+
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("directory", type=Path)
+    parser.add_argument(
+        "-p",
+        "--instance_prompt",
+        default="<cat-toy> toy",
+        help="a prompt that should contain a good description of what your object or style is, together with the initializer word",
+        type=str,
+    )
+    return parser.parse_args()
 
 
 # `pretrained_model_name_or_path` which Stable Diffusion checkpoint you want to use
 pretrained_model_name_or_path = "stabilityai/stable-diffusion-2"
-# Add here the URLs to the images of the concept you are adding. 3-5 should be fine
-urls = [
-    "https://huggingface.co/datasets/valhalla/images/resolve/main/2.jpeg",
-    "https://huggingface.co/datasets/valhalla/images/resolve/main/3.jpeg",
-    "https://huggingface.co/datasets/valhalla/images/resolve/main/5.jpeg",
-    "https://huggingface.co/datasets/valhalla/images/resolve/main/6.jpeg",
-]
-
-
-save_path = Path("./my_concept")
-save_path.mkdir(exist_ok=True)
-for i, url in enumerate(urls):
-    Image.open(BytesIO(requests.get(url).content)).convert("RGB").save(
-        save_path / f"{i}.jpg"
-    )
-# `instance_prompt` is a prompt that should contain a good description of what your object or style is, together with the initializer word `cat_toy`
-instance_prompt = "<cat-toy> toy"  # {type:"string"}
 # Generate Class Images
 # Load the Stable Diffusion model
 # Load models and create wrapper for stable diffusion
@@ -47,12 +40,13 @@ tokenizer = CLIPTokenizer.from_pretrained(
     subfolder="tokenizer",
 )
 # Setting up all training args
-args = Namespace(
+arguments = parse_arguments()
+arguments = Namespace(
     pretrained_model_name_or_path=pretrained_model_name_or_path,
     resolution=vae.sample_size,
     center_crop=True,
-    instance_data_dir=save_path,
-    instance_prompt=instance_prompt,
+    instance_data_dir=arguments.directory / "images",
+    instance_prompt=arguments.instance_prompt,
     learning_rate=5e-06,
     max_train_steps=300,
     save_steps=50,
@@ -67,10 +61,10 @@ args = Namespace(
     num_class_images=12,
     lr_scheduler="constant",
     lr_warmup_steps=100,
-    output_dir=Path("dreambooth-concept"),
+    output_dir=arguments.directory / "dreambooth",
 )
 accelerate.notebook_launcher(
-    train, args=(text_encoder, tokenizer, vae, unet, args), num_processes=1
+    train, args=(text_encoder, tokenizer, vae, unet, arguments), num_processes=1
 )
 for param in itertools.chain(unet.parameters(), text_encoder.parameters()):
     if param.grad is not None:
